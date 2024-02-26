b83860d61f01b3fe39c4447983b20afb1e0e755a
mohamedazizJellazi <aziz.jellazi@gmail.com>
2024-02-26 10:34:04 +0100

    ajout commentaire

--- src/main/java/com/example/examenspringfinal/entities/Worker.java
+++ src/main/java/com/example/examenspringfinal/entities/Worker.java
@@ -15,6 +15,7 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Worker {
+    //worker
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idAgent;
